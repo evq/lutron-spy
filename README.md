c321d27b9d677a963f10405894c18014b78cee6c
Evey Quirk <ev@7pr.xyz>
2015-02-20 10:58:12 -0800

    Initial commit, traces from slsnif -x and corresponding tests

---
+++ README.md
@@ -0,0 +1,63 @@
+# Lutron Spy
+
+Lutron Spy lets you use 
+[Lutron Pico remotes](http://www.amazon.com/Lutron-PJ2-WALL-WH-L01-Remote-Control-Mounting/dp/B00JR202JQ/)
+along with a rooted Wink hub to control any REST interface. It
+listens to the output of a sniffed serial port over which communication
+with the Lutron radio takes place.
+
+## Prerequisites
+
+You must have slsnif already compiled for the Wink hub. I have made
+a buildroot available in the form of a [docker container.](https://github.com/evq/imx28-buildroot)
+
+## How to compile
+
+Install [goxc](https://github.com/laher/goxc)
+
+```bash
+goxc
+```
+
+You can then find the cross compiled binary in 
+
+```
+$GOPATH/bin/lutron-spy-xc/snapshot/linux_arm/lutron-spy
+```
+
+## Configuration
+
+Configuration is done via a json file which specifies which HTTP method,
+url and data to make a request with. Simply copy example-config.json to
+remote-config.json and customize it.
+
+## Usage
+
+```bash
+/etc/init.d/S60lutron-core stop
+vi /etc/lutron.d/lutron.conf
+```
+
+Change the contents to:
+
+```json
+{
+   "DatabasePath":"/database/lutron-db.sqlite",
+   "ServerSocketPath":"/tmp/lutron-core.sock",
+   "LoggingPriority":0,
+   "HardwareInterfaceType":0,
+   "HardwareInterfaceParams":[[0,"/dev/ttyp0"],[1,"115200"]],
+   "SqliteDebugEnabled":1
+}
+```
+
+```bash
+./slsnif /dev/ttySP2 -x | ./lutron-spy &
+/etc/init.d/S60lutron-core start
+```
+
+Remotes can be paired by using the built in aprontest binary.
+
+```bash
+aprontest -a -r lutron
+```
---
+++ lutron-spy_test.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+  "bufio"
+  "github.com/stretchr/testify/assert"
+  "os"
+  "testing"
+  "fmt"
+  "net/http"
+)
+
+type MockTransport struct {
+  needFailNoResponder bool
+}
+
+func (m *MockTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+  return &http.Response{}, nil
+}
+
+func TestMain(m *testing.M) {
+  cf, _ := os.Open("example-config.json")
+  parseConfig(cf)
+
+  client = &http.Client{Transport: &MockTransport{}}
+  os.Exit(m.Run())
+}
+
+func Example_spy_r1_on() {
+  tf, _ := os.Open("traces/AEB551_on.trace")
+  spy(tf)
+  // Output:
+  // serial:  AEB551
+  // button: 2
+  // nickname:  bedside
+  // PUT {"on":true} http://192.168.1.1/api/blah/lights/2/state
+  //
+  // EOF reached.
+}
+
+func Example_spy_r1_up() {
+  tf, _ := os.Open("traces/AEB551_up.trace")
+  spy(tf)
+  // Output:
+  // serial:  AEB551
+  // button: 5
+  // nickname:  bedside
+  //
+  // EOF reached.
+}
+
+func Example_spy_r1_select() {
+  tf, _ := os.Open("traces/AEB551_select.trace")
+  spy(tf)
+  // Output:
+  // serial:  AEB551
+  // button: 3
+  // nickname:  bedside
+  //
+  // EOF reached.
+}
+
+func Test_parseLine_r1_on(t *testing.T) {
+  Helper_parseLine(t, "AEB551", "on")
+}
+
+func Test_parseLine_r2_on(t *testing.T) {
+  Helper_parseLine(t, "AFA592", "on")
+}
+
+func Test_parseLine_r1_off(t *testing.T) {
+  Helper_parseLine(t, "AEB551", "off")
+}
+
+func Test_parseLine_r1_up(t *testing.T) {
+  Helper_parseLine(t, "AEB551", "up")
+}
+
+func Test_parseLine_r1_down(t *testing.T) {
+  Helper_parseLine(t, "AEB551", "down")
+}
+
+func Test_parseLine_r1_select(t *testing.T) {
+  Helper_parseLine(t, "AEB551", "select")
+}
+
+func Helper_parseLine(t *testing.T, remote string, button string) {
+  fh, err := os.Open(fmt.Sprintf("traces/%s_%s.trace", remote, button))
+  if err != nil {
+    t.Error("Missing testing trace")
+  }
+  reader := bufio.NewReader(fh)
+  // First string is a button press
+  line, err := reader.ReadString('\n')
+  if err != nil {
+    t.Error("Corrupted testing trace")
+  }
+  serial, pressed, err := parseLine(line)
+  assert.Equal(t, remote, serial)
+  assert.Equal(t, button, BUTTONS[pressed])
+  assert.Nil(t, err)
+}
